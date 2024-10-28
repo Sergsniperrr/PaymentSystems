f0fa98244c8c1fc6e8fd4eaa7466ea8337e96e08
Stalker <Stalker@NOUT>
2024-10-28 13:46:26 +0300

    Correction_1

--- PaymentSystems/Program.cs
+++ PaymentSystems/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Security.Cryptography;
 
 class Program
@@ -14,15 +11,10 @@ class Program
         //order.system2.ru/pay?hash={MD5 хеш ID заказа + сумма заказа}
         //system3.com/pay?amount=12000&curency=RUB&hash={SHA-1 хеш сумма заказа + ID заказа + секретный ключ от системы}
 
-        MD5HashIdCreator MD5hashCreator = new MD5HashIdCreator();
-        SHA1HashIdCreator SHA1hashCreator = new SHA1HashIdCreator();
-
-        Console.WriteLine(MD5hashCreator.CalculateHash(45));
-        Console.WriteLine(SHA1hashCreator.CalculateHash(45));
-        Console.WriteLine(SHA1hashCreator.CalculateHash(45));
-        Console.WriteLine(MD5hashCreator.CalculateHash(45));
-
-
+        Console.WriteLine(HashCreator.Calculate(45, MD5.Create()));
+        Console.WriteLine(HashCreator.Calculate(45, SHA1.Create()));
+        Console.WriteLine(HashCreator.Calculate(45, SHA1.Create()));
+        Console.WriteLine(HashCreator.Calculate(45, MD5.Create()));
 
         Console.Read();
     }
@@ -46,26 +38,11 @@ interface IHashCreator
     string CalculateHash(int value);
 }
 
-class MD5HashIdCreator : IHashCreator
+static class HashCreator
 {
-    public string CalculateHash(int value)
+    public static string Calculate(int value, HashAlgorithm algoritm)
     {
-        var md5 = MD5.Create();
-        string hash = String.Concat(md5.ComputeHash(BitConverter.GetBytes(value))
+        return String.Concat(algoritm.ComputeHash(BitConverter.GetBytes(value))
                             .Select(x => x.ToString("x2")));
-
-        return hash;
-    }
-}
-
-class HashIdCreator
-{
-    public string CalculateHash(int value, HashAlgorithm hashAlgoritm)
-    {
-        var sha1 = SHA1.Create();
-        string hash = String.Concat(sha1.ComputeHash(BitConverter.GetBytes(value))
-                            .Select(x => x.ToString("x2")));
-
-        return hash;
     }
 }
